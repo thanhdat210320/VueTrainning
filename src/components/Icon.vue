86bf24f96023e1d4346f9a45246fd67b11ff9a8b
thanhdat <thanhdat210320@gmail.com>
2022-08-10 09:31:58 +0700

    Review

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,21 +1,31 @@
 <template>
-    <div class="">
-        <div class="text-[60px] pt-[20px] justify-center flex pl-[8px] pr-[8px]">
-            <font-awesome-icon class="px-[30px] text-[#DD7A01]" icon="fa-solid fa-circle-info" />
-            <font-awesome-icon class="px-[40px] text-[#1858A4]" icon="fa-solid fa-comment-dots" />
-            <font-awesome-icon class="px-[40px] text-[#1858A4]" icon="fa-solid fa-envelope" />
-            <font-awesome-icon class="px-[30px] text-[#DD7A01]" icon="fa-solid fa-square-check" />
-        </div>
+  <div class="">
+    <div class="text-[60px] pt-[20px] justify-center flex pl-[8px] pr-[8px]">
+      <font-awesome-icon
+        class="px-[30px] text-[#DD7A01]"
+        icon="fa-solid fa-circle-info"
+      />
+      <font-awesome-icon
+        class="px-[40px] text-[#1858A4]"
+        icon="fa-solid fa-comment-dots"
+      />
+      <font-awesome-icon
+        class="px-[40px] text-[#1858A4]"
+        icon="fa-solid fa-envelope"
+      />
+      <font-awesome-icon
+        class="px-[30px] text-[#DD7A01]"
+        icon="fa-solid fa-square-check"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"IconComponent"
-
-}
+  name: "IconComponent",
+};
 </script>
 
 <style>
-
 </style>
